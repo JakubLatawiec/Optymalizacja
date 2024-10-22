35ba2adc4300acda3b1c63578334bce55b6f4d17
rafikm <116646888+rafikm2003@users.noreply.github.com>
2024-10-22 11:13:06 +0200

    hookes and rosenbrock with comments

--- src/user_funs.h
+++ src/user_funs.h
@@ -5,5 +5,6 @@
 matrix ff1(matrix, matrix = NAN, matrix = NAN);
 matrix ff0T(matrix, matrix = NAN, matrix = NAN);
 matrix ff0R(matrix, matrix = NAN, matrix = NAN);
+matrix ff2T(matrix, matrix = NAN, matrix = NAN);
 matrix df0(double, matrix, matrix = NAN, matrix = NAN);
 
